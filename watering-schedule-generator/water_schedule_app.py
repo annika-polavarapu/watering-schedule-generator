b84c854df4e9e0e935aff46ec51b62b202958295
Annika Polavarapu <annika@Annikas-MacBook-Air-2.local>
2024-04-10 11:55:25 -0700

    Created the app file and using streamlit for front end

---
+++ watering-schedule-generator/water_schedule_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from schedule_generator import fetchWeatherData, generateWateringSchedule
+
+def main():
+    st.title("Watering Schedule Generator")
+
+
+if __name__ == "__main__":
+    main()
