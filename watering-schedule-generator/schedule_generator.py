6aed4ebc9713aa5c80f9e44cbfd559ad7799d6d7
Annika Polavarapu <annika@Annikas-MacBook-Air-2.local>
2024-04-10 16:21:45 -0700

    Improved algorithm, and taking in more user input

--- watering-schedule-generator/schedule_generator.py
+++ watering-schedule-generator/schedule_generator.py
@@ -1,12 +1,15 @@
 from api_data import fetchWeatherData
 
-def shouldWater(temperature, humidity, precipitationProbability, precipitation, soilMoisture):
+def shouldWater(temperature, humidity, precipitationProbability, precipitation, soilMoisture, wateringNeeds, sunlightExposure):
     # Min and max values for watering
-    minTemperature = 20  
-    maxHumidity = 80     
-    maxPrecipitationProbability = 50 
-    maxPrecipitation = 1  
-    minSoilMoisture = 0.3  
+    minTemperature = 20 - wateringNeeds * 2
+    maxHumidity = 80 + wateringNeeds * 5  
+    maxPrecipitationProbability = 50 - wateringNeeds * 10  
+    maxPrecipitation = 1 - wateringNeeds * 0.2 
+    minSoilMoisture = 0.3 - wateringNeeds * 0.1
+
+    if sunlightExposure == "shade":
+        maxHumidity -= 5
 
     # Checking if any of the conditions are met to need watering
     if temperature > minTemperature \
@@ -18,23 +21,24 @@ def shouldWater(temperature, humidity, precipitationProbability, precipitation, 
     else:
         return False
 
-def generateWateringSchedule(weatherData):
+def generateWateringSchedule(weatherData, wateringNeeds, sunlighExposure, lastWateredDay):
     # Extracting weather variables from the data for 7 days
-    temperature = weatherData['temperature'][:7]
-    humidity = weatherData['humidity'][:7]
-    precipitationProbability = weatherData['precipitationProbability'][:7]
-    precipitation = weatherData['precipitation'][:7]
-    soilMoisture = weatherData['soilMoisture'][:7]
+    temperature = weatherData['temperature']#[:7]
+    humidity = weatherData['humidity']#[:7]
+    precipitationProbability = weatherData['precipitationProbability']#[:7]
+    precipitation = weatherData['precipitation']#[:7]
+    soilMoisture = weatherData['soilMoisture']#[:7]
 
     # Generating watering schedule
     wateringSchedule = []
     for day in range(len(temperature)):
-        if shouldWater(temperature[day], humidity[day], precipitationProbability[day], precipitation[day], soilMoisture[day]):
+        if shouldWater(temperature[day], humidity[day], precipitationProbability[day], precipitation[day], soilMoisture[day], wateringNeeds, sunlighExposure):
             wateringSchedule.append(f"Day {day + 1}: Watering needed")
+            lastWateredDay = day
         else:
             wateringSchedule.append(f"Day {day + 1}: No watering needed")
     
-    return wateringSchedule
+    return wateringSchedule, lastWateredDay
 
 def main():
     useHardcodedData = True
@@ -55,8 +59,24 @@ def main():
         weatherData = fetchWeatherData(latitude, longitude)
 
     if weatherData:
+        # Getting user input
+        wateringNeeds = input("Enter watering needs (low, medium, high): ")
+        sunlightExposure = input("Enter sunlight exposure (shade, sun): ")
+
+        if wateringNeeds == "low":
+            wateringNeeds = 1
+        elif wateringNeeds == "medium":
+            wateringNeeds = 2
+        elif wateringNeeds == "high":
+            wateringNeeds = 3
+        else:
+            print("Invalid watering needs. Defaulting to medium.")
+            wateringNeeds = 2
+        
+        lastWateredDay = -1
+        
         # Generating watering schedule
-        wateringSchedule = generateWateringSchedule(weatherData)
+        wateringSchedule, lastWateredDay = generateWateringSchedule(weatherData, wateringNeeds, sunlightExposure, lastWateredDay)
         print("Watering Schedule for the Next 7 Days:")
         for entry in wateringSchedule:
             print(entry)
